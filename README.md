76692cbf18cbdc64a9640c90229ac36ec5f62351
Jason M. Pittman <jason@jasonmpittman.com>
2020-07-13 10:06:53 -0400

    initial commit for ver 0.9.0

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# ovirt-console
-Parse console.vv and partially automate connecting to Ovirt virtual machines with VNC enabled.
+# Ovirt Console
+Ovirt Console parses a console.vv file and partially automates connecting to Ovirt virtual machines with VNC enabled.
+
+Ovirt Console was developed because of the inability for popular VNC viewer applications to read the *console.vv* file associated with remotely accessible virtual machines running in Ovirt. The program automates running the installed VNC viewer and displaying the associated password.
+
+## Prerequisites for Ovirt Console
+Ovirt Console requires:
+- Python 3
+- A VNC viewer such as [RealVNC](https://www.realvnc.com/en/) or [TigerVNC](https://tigervnc.org/)
+
+## How to Setup Ovirt Console
+- Clone this repository as `git clone https://github.com/jasonmpittman/ovirt-console`
+
+## How to Use Ovirt Console
+1. Click on the *console* option your Ovirt instance.
+2. Download and save the console.vv file.
+  - **Important**: save the file in the ovirt-console repo directory
+3. From a shell / terminal, execute Ovirt Console as `python3 ovirt-console`
+  - Two things will happen. First, the VNC viewer will be executed. Second, the Ovirt set password will be displayed in the shell / terminal.
+4. Copy the password from the shell and paste into the VNC viewer application. 
+5. Click *connect* or press Enter.
+
+## Testing
+Ovirt Console has been tested using Python 3.6.9 on Ubuntu 18.04.4 LTS and the same running as Windows Subsystem for Linux.
---
+++ ovirt-console.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+"""Parse console.vv and partially automate connecting to Ovirt virtual machines with VNC enabled"""
+
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2020"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "GPLv3"
+__version__ = "0.9.0"
+__maintainer__ = "Jason M. Pittman"
+__email__ = "jpittman@highpoint.edu"
+__status__ = "Development"
+
+from pathlib import Path
+import subprocess
+import sys
+import configparser
+
+config_file = 'config.ini'
+
+'''
+Get the vnc configuration details
+'''
+def get_vnc_config(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    vnc_file = config['vnc']['viewer-file']
+    vnc_folder = Path(config['vnc']['viewer-folder'])
+    vnc_viewer = vnc_folder / config['vnc']['viewer-exe']
+
+    return vnc_file, vnc_folder, vnc_viewer
+
+'''
+Get the connection details for the remote system
+'''
+def get_vnc_parameters(vnc_file):
+    console_file = configparser.ConfigParser()
+    console_file.read(vnc_file)
+
+    host = console_file['virt-viewer']['host']
+    port = console_file['virt-viewer']['port']
+    password = console_file['virt-viewer']['password']
+
+    return host, port, password
+
+'''
+Run our local vnc viewer against the remote system
+'''
+def run_vnc(vnc_file, vnc_viewer):
+    host, port, password = get_vnc_parameters(vnc_file)
+    print(password)
+
+    exec_viewer = str(vnc_viewer) 
+    exec_options = ' ' + host + ':' + port 
+
+    subprocess.run([exec_viewer,exec_options])
+
+'''
+Entry point into our program
+'''
+def main():
+    vnc_file, vnc_folder, vnc_viewer = get_vnc_config(config_file)
+    run_vnc(vnc_file, vnc_viewer)
+    
+
+main()
